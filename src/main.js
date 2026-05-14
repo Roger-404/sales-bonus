ac1a4e4bd873819a3935bce1be7027028938e1f5
Egor Shubin <eshubin15@gmail.com>
2026-05-15 01:41:27 +0300

    Fourth version of the program

--- src/main.js
+++ src/main.js
@@ -102,7 +102,7 @@ function analyzeSalesData(data, options) {
             if (!seller.products_sold[item.sku]) {
                 seller.products_sold[item.sku] = 0;
             }
-            seller.products_sold[item.sku]++
+            seller.products_sold[item.sku] += item.quantity
         });
     });
 
